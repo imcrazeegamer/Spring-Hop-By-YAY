5e20c2ca37aa6942467fedc97005aed64b044964
imcrazeegamer <imcrazeegamer@gmail.com>
2019-02-05 12:31:31 +0200

    Added The Game It Self
    Can Jump and has infinite level (no animations)

---
+++ Assets/DeadZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Player")
+        {
+            //Dead
+            collision.gameObject.GetComponent<Rigidbody2D>().simulated = false;
+            Debug.Log("Dead");
+        }
+    }
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    [SerializeField]
+	private GameObject platformPrefab;
+
+    [SerializeField]
+    private float levelWidth = 3f;
+    [SerializeField]
+    private float minY = .2f;
+    [SerializeField]
+    private float maxY = 1.5f;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Platform")
+        {
+            Debug.Log("Platform off Screen");
+            CreatePlatform();
+            Destroy(collision.gameObject);
+        }
+           
+    }
+
+    private void CreatePlatform() {
+        GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
+        Vector3 playerPosition = GameObject.FindWithTag("Player").transform.position;
+        float maxdis = platforms.ToList().Max((p) => p.transform.position.y);
+        Vector3 spawnPosition = platforms.ToList().Where((p) => p.transform.position.y == maxdis).ToList()[0].transform.position;
+        Quaternion rotate = Quaternion.Euler(0, 0, 90);
+        spawnPosition.y += Random.Range(minY, maxY);
+	    spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+		Instantiate(platformPrefab, spawnPosition, rotate);
+		
+	}
+}
